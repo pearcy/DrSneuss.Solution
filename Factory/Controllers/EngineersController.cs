8362d0151bf1e20cee26b3340abc323a7521fafe
Joseph Pearce <joseph.pearce05@gmail.com>
2020-08-07 11:23:01 -0700

    add create html & controller code

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -21,5 +21,23 @@ namespace Factory.Controllers
       return View(_db.Engineers.ToList());
     }
 
+    public ActionResult Create()
+   {
+     ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+     return View();
+   }
+
+   [HttpPost]
+   public ActionResult Create(Engineer engineer, int MachineId)
+   {
+     _db.Engineers.Add(engineer);
+     if (MachineId != 0)
+     {
+       _db.MachineEngineer.Add(new MachineEngineer() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+     }
+     _db.SaveChanges();
+     return RedirectToAction("Index");
+   }
+
   }
 }
